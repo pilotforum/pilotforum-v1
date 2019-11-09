c612992d95b9f8a279f8be13c606adb38e1d45c5
ipdmartins <ipdmartins@gmail.com>
2019-11-09 19:29:03 -0300

    create pages

--- client/student/pages/subjects/index.js
+++ client/student/pages/subjects/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from '~/services/api';
 import { DefaultLayout } from '~/components/Layout';
+import Box from '~/components/Box';
 
 export default function Disciplines() {
+  const [disciplines, setDisciplines] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getDisciplines() {
+      const { data } = await api.get('/subjects');
+      setDisciplines(data);
+      setLoading(false);
+      console.log(data);
+    }
+
+    getDisciplines();
+  }, []);
+
   return (
     <DefaultLayout>
-      <h1>Disciplines</h1>
+      <h1>Todas as matérias</h1>
+      {loading
+        ? 'carregando...'
+        : disciplines.map((subject) => (
+            <Box key={subject.id} name={subject.name} />
+          ))}
     </DefaultLayout>
   );
 }
--- client/student/pages/tags/index.js
+++ client/student/pages/tags/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from '~/services/api';
+import Link from 'next/link';
 import { DefaultLayout } from '~/components/Layout';
+import Box from '~/components/Box';
 
 export default function Tags() {
+  const [tags, setTags] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getTags() {
+      const { data } = await api.get('/tags');
+      setTags(data);
+      setLoading(false);
+      console.log(data);
+    }
+
+    getTags();
+  }, []);
+
   return (
     <DefaultLayout>
       <h1>Tags</h1>
+      {loading
+        ? 'carregando...'
+        : tags.map((tag) => (
+            <Link key={tag.id} href={`tag/${tag.name}`}>
+              <a style={{ width: '100%' }}>
+                <Box name={tag.name} />
+              </a>
+            </Link>
+          ))}
     </DefaultLayout>
   );
 }
